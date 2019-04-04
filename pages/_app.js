f19fd4ceb5ffc385c99c5e76c79d4e77e5815a16
nemoto <nemoto@enfunctor.com>
2019-04-05 00:30:05 +0900

    feat: add FontAwesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import App from 'next/app'
 import '../styles/global.scss'
+import '@fortawesome/fontawesome-free/js/fontawesome.min'
+import '@fortawesome/fontawesome-free/js/solid.min'
 
 export default App
