553e1573883c603b7cbcf317175931f35c8e5feb
nemoto <nemoto@enfunctor.com>
2019-04-07 23:56:33 +0900

    chore: turn IndexPage into Class Component for testability

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,92 @@
-import { useRef, useState } from 'react'
+import { Component, createRef } from 'react'
 import { Head } from '../components/head'
 import { TodoList } from '../components/todo-list'
 import { DeleteModal, DeleteModalRef } from '../components/delete-modal'
 import { TodoAdd } from '../components/todo-add'
+import { Todo } from '../types/todo'
 
-export default () => {
-  const [list, setList] = useState([
-    { id: 1, done: true, name: 'Buy a milk for my boss' },
-    { id: 2, done: false, name: 'Send a mail to a client' },
-  ])
-
-  const [counter, setCounter] = useState(3)
-
-  const add = name => {
-    setList(list.concat({ id: counter, name, done: false }))
-    setCounter(counter + 1)
-  }
-
-  const remove = todo => setList(list.filter(({ id }) => id !== todo.id))
-
-  const toggle = id => {
-    const newList = [...list]
-    const target = newList.find(todo => todo.id === id)
-    if (target) target.done = !target.done
-    setList(newList)
-  }
-
-  let deleteModalRef = useRef<DeleteModalRef>()
-
-  return (
-    <>
-      <Head title="next-todo" />
-      <section className="hero">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">
-              <i className="fa fa-clipboard-list" />
-              <span className="pl-1">next-todo</span>
-            </h1>
-            <h2 className="subtitle">A todo list manager made with Next.js</h2>
-          </div>
-        </div>
-      </section>
-      <section className="container">
-        <TodoAdd onAdd={add} />
-        <TodoList
-          todos={list}
-          onToggle={toggle}
-          onClickRemove={todo => {
-            return deleteModalRef.current.show(todo)
-          }}
-        />
-      </section>
-      <DeleteModal ref={deleteModalRef} onRemove={remove} />
-    </>
-  )
+export interface IndexProps {
+  state?: IndexState
 }
+
+export interface IndexState {
+  list: Todo[]
+  counter: number
+}
+
+class IndexPage extends Component {
+  props: IndexProps
+  state: IndexState
+
+  constructor(props) {
+    super(props)
+    this.state = this.initialState
+  }
+
+  get initialState(): IndexState {
+    return {
+      list: [
+        { id: 1, done: true, name: 'Buy a milk for my boss' },
+        { id: 2, done: false, name: 'Send a mail to a client' },
+      ],
+      counter: 3,
+    }
+  }
+
+  get list() {
+    return this.state.list
+  }
+  get counter() {
+    return this.state.counter
+  }
+  setList = (list: Todo[]) => this.setState(state => ({ ...state, list }))
+  setCounter = counter => this.setState(state => ({ ...state, counter }))
+  add = name => {
+    this.setList(
+      this.state.list.concat({ id: this.counter, name, done: false })
+    )
+    this.setCounter(this.counter + 1)
+  }
+  remove = todo => this.setList(this.list.filter(({ id }) => id !== todo.id))
+  toggle = id => {
+    const newList = [...this.list]
+    const target = newList.find(todo => todo.id === id)
+    if (!!target) target.done = !target.done
+    this.setList(newList)
+  }
+
+  deleteModalRef = createRef<DeleteModalRef>()
+  showModal = (todo: Todo) => this.deleteModalRef.current.show(todo)
+
+  render() {
+    return (
+      <>
+        <Head title="next-todo" />
+        <section className="hero">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                <i className="fa fa-clipboard-list" />
+                <span className="pl-1">next-todo</span>
+              </h1>
+              <h2 className="subtitle">
+                A todo list manager made with Next.js
+              </h2>
+            </div>
+          </div>
+        </section>
+        <section className="container">
+          <TodoAdd onAdd={this.add} />
+          <TodoList
+            todos={this.list}
+            onToggle={this.toggle}
+            onClickRemove={this.showModal}
+          />
+        </section>
+        <DeleteModal ref={this.deleteModalRef} onRemove={this.remove} />
+      </>
+    )
+  }
+}
+
+export default IndexPage
