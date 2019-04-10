3984dc1d65a4c62924ca35c80d257577195f6c00
nemoto <nemoto@enfunctor.com>
2019-04-10 23:46:09 +0900

    test: add test for TodoAdd

---
+++ components/todo-add.spec.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { TodoAdd } from './todo-add'
+
+describe('TodoAdd', () => {
+  let wrapper
+  const onAddSpy = jest.fn()
+
+  beforeEach(() => {
+    wrapper = shallow(<TodoAdd onAdd={onAddSpy} />)
+  })
+
+  test('should disable button if input field is empty', () => {
+    expect(wrapper.find('button').is('[disabled]')).toBeTruthy()
+  })
+
+  test('should call onRenderSpy on click add button', () => {
+    wrapper.find('input').simulate('change',{target:{value:'dummy name'}})
+    wrapper.find('form').simulate('submit', new MouseEvent('click'))
+    expect(onAddSpy).toBeCalledWith('dummy name')
+  })
+})
